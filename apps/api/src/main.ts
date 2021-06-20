e5ca962c1b21ca8a2d69082622d3ab879052cc94
wscnd <72019998+wscnd@users.noreply.github.com>
2021-06-20 12:13:45 -0300

    feat(express): search pokemons by name route

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,12 +4,32 @@
  */
 
 import * as express from 'express';
+import { Request } from 'express-serve-static-core';
+
+import { pokemon } from './data';
 
 const app = express();
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to api!' });
 });
+
+app.get('/pokemon', (req, res) => {
+  res.send(pokemon);
+});
+
+app.get(
+  '/search',
+  (req: Request<unknown, unknown, unknown, { q: string }>, res) => {
+    const query = (req.query.q || '') /** fallback to not using query */
+      .toLowerCase();
+    const search = pokemon.filter(({ name: { english } }) => {
+      return english.toLowerCase().includes(query);
+    });
+
+    res.send(search);
+  }
+);
 
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
